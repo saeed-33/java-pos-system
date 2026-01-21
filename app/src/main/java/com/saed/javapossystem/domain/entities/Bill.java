331234df0a98ffa29be98900513556f0041ab408
Abd-Nash <61282985+abd-nash@users.noreply.github.com>
2026-01-21 14:59:17 +0300

    feat: Implement entity classes
    Implement constructors, getters, and final fields for the `Product`, `Bill`, and `BillRow` entity classes. This change makes the entities immutable.

--- app/src/main/java/com/saed/javapossystem/domain/entities/Bill.java
+++ app/src/main/java/com/saed/javapossystem/domain/entities/Bill.java
@@ -1,12 +1,38 @@
 package com.saed.javapossystem.domain.entities;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Bill {
     private int id;
     private Date date;
     private double totalPrice;
+
+    public Bill(int id, String date, double totalPrice) {
+        this.id = id;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        try{
+            this.date = sdf.parse(date);
+        }catch (ParseException e){
+            this.date = new Date();
+        }
+        this.totalPrice = totalPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
 
 
 }
--- app/src/main/java/com/saed/javapossystem/domain/entities/Product.java
+++ app/src/main/java/com/saed/javapossystem/domain/entities/Product.java
@@ -1,9 +1,41 @@
 package com.saed.javapossystem.domain.entities;
 
+
 public class Product {
-    private int id;
-    private String name;
-    private int quantity;
-    private double price;
-    private String code;
+    final private int id;
+    final private String name;
+    final private int quantity;
+    final private double price;
+    final private String code;
+
+    public Product(int id, String name, int quantity, double price, String code) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.code = code;
+    }
+    public Product(String name, int quantity, double price, String code){
+        this.id = -1;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getCode() {
+        return code;
+    }
 }
